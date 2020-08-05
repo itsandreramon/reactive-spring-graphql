e352e44f3e9093ac4803b3704135a1eb97510029
andrethiele <thielea@th-brandenburg.de>
2020-08-06 01:03:13 +0200

    small updates + add docker-compose.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,11 +24,12 @@ dependencies {
   // Kotlin
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
   // Reactor
-  implementation("io.projectreactor:reactor-tools")
+  // implementation("io.projectreactor:reactor-tools")
   implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 
   // Postgres
