dd25ec094784c487f0e0b25b2253710c6d868362
andrethiele <thielea@th-brandenburg.de>
2020-08-05 17:46:01 +0200

    fix column names

--- src/main/kotlin/com/andreramon/demo/model/Book.kt
+++ src/main/kotlin/com/andreramon/demo/model/Book.kt
@@ -1,12 +1,20 @@
 package com.andreramon.demo.model
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("books")
 data class Book(
-  @Id val id: Int,
+  @Id
+  val id: Int,
+
+  @Column("name")
   val name: String?,
+
+  @Column("pagecount")
   val pageCount: Int?,
+
+  @Column("authorid")
   val authorId: Int?
 )
