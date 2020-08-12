c276406385f4a28cabd561a57b13be1fbd28de4f
andrethiele <thielea@th-brandenburg.de>
2020-08-12 11:21:10 +0200

    add author service test

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,35 +53,31 @@ jacoco {
 }
 
 dependencies {
-
-    // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.experimental:spring-graalvm-native:0.7.1")
 
-    // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-    // Reactor
     // implementation("io.projectreactor:reactor-tools")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 
-    // Postgres
     runtimeOnly("io.r2dbc:r2dbc-postgresql")
     runtimeOnly("org.postgresql:postgresql")
 
-    // GraphQL
     implementation("com.expediagroup:graphql-kotlin-spring-server:3.6.1")
 
-    // Test
+    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("io.kotest:kotest-assertions-core:4.1.3")
+    testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("io.projectreactor:reactor-test")
+
 }
 
 tasks {
---
+++ src/test/kotlin/com/andreramon/demo/service/AuthorServiceTest.kt
@@ -0,0 +1,31 @@
+package com.andreramon.demo.service
+
+import com.andreramon.demo.model.Author
+import com.andreramon.demo.repository.AuthorRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+
+class AuthorServiceTest {
+
+    private val authorRepository = mockk<AuthorRepository>()
+    private val authorService = AuthorService(authorRepository)
+
+    @BeforeEach
+    fun setup() {
+        every {
+            authorRepository.findById(1)
+        } returns Mono.just(Author(1, "Leo Tolstoy"))
+    }
+
+    @Test
+    fun testFindAuthorById() {
+        StepVerifier.create(authorService.findAuthorById(1))
+                .expectNextMatches { it == Author(1, "Leo Tolstoy") }
+                .expectComplete()
+                .verify()
+    }
+}
