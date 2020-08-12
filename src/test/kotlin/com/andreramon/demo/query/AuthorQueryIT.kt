f8b7159d37c6588f45ca63d0c2a35dd5c64d6b30
andrethiele <thielea@th-brandenburg.de>
2020-08-12 11:37:47 +0200

    add book integration test

--- src/test/kotlin/com/andreramon/demo/query/AuthorQueryIT.kt
+++ src/test/kotlin/com/andreramon/demo/query/AuthorQueryIT.kt
@@ -2,30 +2,29 @@ package com.andreramon.demo.query
 
 import com.andreramon.demo.GRAPHQL_ENDPOINT
 import com.andreramon.demo.GRAPHQL_MEDIA_TYPE
-import com.andreramon.demo.model.Book
+import com.andreramon.demo.model.Author
 import com.andreramon.demo.verifyData
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest
 @AutoConfigureWebTestClient
-@TestInstance(PER_CLASS)
-class BookQueryIT(@Autowired private val testClient: WebTestClient) {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class AuthorQueryIT(@Autowired private val testClient: WebTestClient) {
 
     @Test
-    fun testBookByIdQuery() {
-        val query = "bookById"
-        val data = Book(1, "Anna Karenina", 1)
+    fun testAuthorByIdQuery() {
+        val query = "authorById"
+        val data = Author(1, "Leo Tolstoy")
 
         testClient.post()
             .uri(GRAPHQL_ENDPOINT)
-            .accept(APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
             .contentType(GRAPHQL_MEDIA_TYPE)
             .bodyValue("query { $query(id: 1) { name } }")
             .exchange()
